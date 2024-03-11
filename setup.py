aa39754276ce0146a9c50de6f4bd858e0ec79200
Dali Kewara <dalikewara@gmail.com>
2024-03-11 17:09:38 +0700

    update some scripts

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='ayapingping-py',
-    version='4.3.7',
+    version='4.3.8',
     python_requires='>=3.10.12',
     author='Dali Kewara',
     author_email='dalikewara@gmail.com',
